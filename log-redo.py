e947c05aa4741bd06d6c2164db7fa67643eac5be
IgorRadtke <igor.radtke@hotmail.com>
2022-06-07 16:23:44 -0300

    conectando banco

--- log-redo.py
+++ log-redo.py
@@ -1,3 +1,21 @@
+import psycopg2
+
+#Conectando Banco Postgres
+def conectandoBanco():
+    conn = psycopg2.connect(
+    host='localhost'
+    dbname='TrabalhoLog'
+    user='postgres'
+    password='1234'
+    )
+    print('conectado') #para teste
+    return conn
+
+def conecta(con, sql):
+    cur = con.cursor()
+    cur.execute(sql)
+    con.commit()
+
 def openFile {
 
     print("Opening file...")
