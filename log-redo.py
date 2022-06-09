713689c37952fe8fb33fb44f85413946bd12c4f4
IgorRadtke <igor.radtke@hotmail.com>
2022-06-08 22:01:45 -0300

    Nova função inserindo registro, alteração na função executa_db

--- log-redo.py
+++ log-redo.py
@@ -17,7 +17,6 @@ def conectandoBanco():
 def executa_db(sql):
     con = conectandoBanco()
     cur = con.cursor()
-<<<<<<< HEAD
     try:
         cur.execute(sql)
         con.commit()
@@ -26,21 +25,12 @@ def executa_db(sql):
         con.close()
         return 1
     cur.close()
-=======
-    cur.execute(sql)
-    con.commit()
-    con.close()
-
-
-
->>>>>>> cddeac7ef90458034ce1918a184f62af91ff1c25
 
 # Drop da tabela caso ela ja exista 
 sql = 'DROP TABLE IF EXISTS log'
 executa_db(sql)
 
 #Criar Tabela log 
-<<<<<<< HEAD
 sql = 'CREATE TABLE log'
 executa_db(sql)
 
@@ -49,13 +39,6 @@ executa_db(sql)
 def insereBanco( id, A, B ):
     sql = """ INSERT INTO log (id, colunaA, colunaB) VALUES ('%d','%d','%d'); """ % (id, A, B)
     executa_db(sql)
-=======
-sql = 'CREATE TABLE log (id INTEGER, colunaA INTEGER, colunaB INTEGER)'
-executa_db(sql)
-
-  
-
->>>>>>> cddeac7ef90458034ce1918a184f62af91ff1c25
 
 
 """ Abrindo arquivo de log """
