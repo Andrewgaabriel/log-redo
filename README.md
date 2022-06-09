3159b6a427dadb8db3ead99a7049f3a7298e53fa
Andrew <andrew.gabrielgomes@gmail.com>
2022-06-08 22:58:43 -0300

    README edits + Correções de Identação + Resolução de Merge

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# log-redo
-Implementação do mecanismo de log REDO com checkpoint usando o SGBD em Python
+# **Implementação do mecanismo de Log REDO com checkpoint usando PostgreSQL em Python**
+
+*Implementado por: [Andrew Gabriel](https://github.com/Andrewgaabriel) e [Igor Radkte](https://github.com/IgorRadtke)*
+
+*******
+
+
+## **Conteúdo:**
+1. *[Funções Implementadas](#funções)*
+2. *[Como testar/executar ?](#como-testar)*
+3. *[Como funciona o mecanismo de Log REDO ?](#mecanismo-de-log-redo)*
+4. *[Requisitos](#requisitos)*
+
+
+*******
+
+
+
+
+
+<div id='funções'/>  
+
+## **Funções Implementadas:**
+
+- *Nome da função + O que ela faz*  
+`codigo aqui`
+
+- *Nome da função + O que ela faz*  
+`codigo aqui`
+
+- *Nome da função + O que ela faz*  
+`codigo aqui`
+
+
+
+
+<div id='como-testar'/>
+
+## **Como testar/executar:**
+
+- *Como executar/descrição de como executar*  
+`codigo aqui`
+
+
+
+
+<div id='mecanismo-de-log-redo'/>
+
+## **Funcionamento:**
+
+- *Descrição do mecanismo de Log REDO*
+
+<div id='requisitos'/>
+
+## **Requisitos do trabalho acadêmico:**
+
+- **Funções a serem implementadas:**    
+
+    1. Carregar o banco de dados com a tabela antes de executar o código do log (para zerar as configurações e dados parciais);  
+
+    2. Carregar o arquivo de log;
+
+    3. Verifique quais transações devem realizar REDO. Imprimir o nome das transações que irão sofrer Redo. Observem a questão do checkpoint;
+
+    4. Checar quais valores estão salvos nas tabelas (com o select) e atualizar valores inconsistentes (update);
+
+    5. Reportar quais dados foram atualizados;
+
+    6. Seguir o fluxo de execução conforme o método de REDO, conforme visto em aula;
+
+- **Execução:**
+    1. Pode ser implementado em duplas;
+
+    2. A nota será individual;
+
+    3. Deve ser enviado o repositório no GIT (será avaliado a participação dos membros através dos commits). Um único commit com o código pronto será entendido como uma cópia e receberá nota zero.  Os commits irão interferir na nota final dos membros da dupla;
+
+    4. Será testado com outro arquivo de log a execução do programa;
+
--- log-redo.py
+++ log-redo.py
@@ -3,7 +3,7 @@ import psycopg2
 import sys
 
 
-""" Faz a conexão com o Banco de Dados """
+""" ------------------Faz a conexão com o Banco de Dados---------------------------------------- """
 def conectandoBanco():
     con = psycopg2.connect( host='localhost',
                             dbname='trabalholog',
@@ -11,9 +11,11 @@ def conectandoBanco():
                             password='1234')
     return con
 
+""" -------------------------------------------------------------------------------------------- """
 
 
-""" Executa algum comando SQL no banco de dados """
+
+""" -------------------Executa algum comando SQL no banco de dados-------------------------------- """
 def executa_db(sql):
     con = conectandoBanco()
     cur = con.cursor()
@@ -26,22 +28,21 @@ def executa_db(sql):
         return 1
     cur.close()
 
-"""  Drop da tabela caso ela ja exista  """
-sql = 'DROP TABLE IF EXISTS log'
-executa_db(sql)
-
-"""  Criar Tabela log  """
-sql = 'CREATE TABLE log'
-executa_db(sql)
+""" -------------------------------------------------------------------------------------------- """
 
 
-"""  Inserindo Registro """
+
+""" --------------Inserindo Registro------------------------------------------------------------- """
+
 def insereBanco( id, A, B ):
     sql = """ INSERT INTO log (id, colunaA, colunaB) VALUES ('%d','%d','%d'); """ % (id, A, B)
     executa_db(sql)
 
+""" -------------------------------------------------------------------------------------------- """
 
-""" Abrindo arquivo de log """
+
+
+""" -----------------Abrindo arquivo de log------------------------------------------------------- """
 def openFile(fileName):
     try:
         file = open(fileName, 'r')
@@ -50,27 +51,39 @@ def openFile(fileName):
     except:
         print('Erro ao abrir arquivo')
 
+""" -------------------------------------------------------------------------------------------- """
 
-""" Função para imprimir o conteúdo do arquivo """
+
+
+""" -----------------------Função para imprimir conteúdo---------------------------------------- """
 def printFile(file):
     for line in file:
         print(line)
 
+""" -------------------------------------------------------------------------------------------- """
 
-""" Função que pega o nome do arquivo de entrada"""
+
+
+""" --------------------Função que pega o nome do arquivo de entrada------------------------------ """
 def getParam():
     return sys.argv[1]
 
+""" -------------------------------------------------------------------------------------------- """
 
-""" Pega os dados do arquivo e coloca em um vetor """
+
+
+""" ----------------Pega os dados do arquivo e coloca em um vetor-------------------------------- """
 def getData(file):
     data = []
     for line in file:
         data.append(line)
     return data
 
+""" -------------------------------------------------------------------------------------------- """
 
-""" Função que pega os dados necessários para construir/inicializar a tabela """
+
+
+""" -----------------Função que pega os dados necessários para construir/inicializar a tabela----- """
 def getInitTable(data):
     initTable = []
 
@@ -82,8 +95,11 @@ def getInitTable(data):
     
     return initTable
 
+""" -------------------------------------------------------------------------------------------- """
 
-""" Função que pega os dados necessários para efetuar o redo """
+
+
+""" -------------------Função que pega os dados necessários para efetuar o redo------------------ """
 def getRedoInfos(data):
     redoInfos = []
     data.reverse()
@@ -96,6 +112,8 @@ def getRedoInfos(data):
     
     return redoInfos
 
+""" -------------------------------------------------------------------------------------------- """
+
 
 
 
@@ -103,23 +121,28 @@ def getRedoInfos(data):
 
 
 """ 
-Função que divide os dados em: -Dados para fazer o REDO e - Dados para preencher a tabela
-def splitData(data):
-    for line in data:
-        line = line.split(';')
-        return line
-
-    
     FALTA FAZER:
 
-    - Splitar os dados do arquivo em:
-        -- Dados de inserção na tabela
-        -- Dados do log 
+    - Função que inicialize a tabela (juntar as operações SQL ja feitas em uma função)
     - Função que percorre o arquivo de dados do log
     - Função que percorre o arquivo de dados de inserção na tabela
     - Funções que o professor pediu para fazer
 
  """
+
+
+
+""" -----------------------EXECUÇÃO PRINCIPAL--------------------------------------------------------------------- """
+
+
+"""  Drop da tabela caso ela ja exista  """
+sql = 'DROP TABLE IF EXISTS log'
+executa_db(sql)
+
+
+"""  Criar Tabela log  """
+sql = 'CREATE TABLE log (id INTEGER PRIMARY KEY, colunaA INTEGER, colunaB INTEGER);'
+executa_db(sql)
 
 
 file = openFile(getParam()) # Abrindo arquivo de log
