5b442aac359fbbd8f0bd6d43754fa3121abcea47
Andrew <andrew.gabrielgomes@gmail.com>
2022-06-13 19:25:29 -0300

    Função que pega os dados para inicializar a tabela

--- log-redo.py
+++ log-redo.py
@@ -37,7 +37,6 @@ def executa_db(sql):
 def insereBanco( id, A, B):
     sql = """ INSERT INTO log (id, colunaA, colunaB) VALUES ('%d','%d','%d'); """ % (id, A, B)
     executa_db(sql)
-    #for ...
 """ -------------------------------------------------------------------------------------------- """
 
 
@@ -117,19 +116,52 @@ def getRedoInfos(data):
 
 
 
+def getLinha(id, linhas):
+    for linha in linhas:
+        if linha.id == id:      
+            return linha
+    
+    return None 
+    
+
+def parserInfoInit(infoInit):
+
+    linhas = []
+
+    for line in infoInit:
+        print(line)
+        linha = Linha()
+
+        line1 = line.split(',')
+
+        coluna = line1[0]
+        line2 = line1[1].split('=')
+        valor = int(line2[1])
+        id = int(line2[0])
 
 
+        linhaTemp = getLinha(id, linhas)
 
-""" 
-    FALTA FAZER:
+        if linhaTemp == None:
 
-    - Função que inicialize a tabela (juntar as operações SQL ja feitas em uma função)
-    - Função que percorre o arquivo de dados do log
-    - Função que percorre o arquivo de dados de inserção na tabela
-    - Funções que o professor pediu para fazer
 
- """
+            linha.setId(id)
 
+            if coluna == 'A':
+                linha.setColunaA(valor)
+            elif coluna == 'B':
+                linha.setColunaB(valor)
+            
+            linhas.append(linha)
+        else:
+            if coluna == 'A':
+                linhaTemp.setColunaA(valor)
+            elif coluna == 'B':
+                linhaTemp.setColunaB(valor)
+    
+    return linhas
+        
+            
 
 
 """ -----------------------EXECUÇÃO PRINCIPAL--------------------------------------------------------------------- """
@@ -149,6 +181,7 @@ file = openFile(getParam()) # Abrindo arquivo de log
 data = getData(file) # Pegando dados do arquivo e colocando em um vetor
 initTable = getInfoInit(data) # Pegando os dados para preencher a tabela
 redoInfos = getRedoInfos(data) # Pegando os dados para fazer o REDO
+tuplas = parserInfoInit(initTable)
 
 
 
@@ -158,6 +191,7 @@ print("\nDados para adicionar na tabela:\n")
 printFile(initTable)
 print("\nDados para fazer o REDO:\n")
 printFile(redoInfos)
+print("\nDados parseados\n")
 
 
 
